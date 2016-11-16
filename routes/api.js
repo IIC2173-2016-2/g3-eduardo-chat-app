0445ef339fb6d4748076e3f47b52370706108c54
Óscar Estay <oaestay@uc.cl>
2016-11-16 20:07:04 -0300

    Changed headers & bad-request

--- routes/api.js
+++ routes/api.js
@@ -41,47 +41,56 @@ module.exports = (client, mongoose) => {
 
   router.route('v1/is_chat_created').get(
     (req, res, next) => {
-      const chat_id = req.get('chat_id');
-      if (process.env.LOAD === "heavy"){
-        mongoose.model('Backup').findOne({ id: chat_id }, (err, chat) => {
+      const chat_id = req.get('CHAT-ID');
+      if (chat_id === undefined){
+        res.status(400).send({ message: "Bad request." });
+      }
+      else {
+        if (process.env.LOAD === "heavy"){
+          mongoose.model('Backup').findOne({ id: chat_id }, (err, chat) => {
+            if (err) throw err;
+            if (chat) {
+              res.status(200).send({ message: `Found chat ${chat_id}.` });
+            }
+          });
+        };
+        mongoose.model('Chat').findOne({ id: chat_id }, (err, chat) => {
           if (err) throw err;
+          if (!chat) {
+            res.status(404).send({ message: `Chat not found.` });
+          }
           if (chat) {
-            res.status(200).send({ message: `Found chat ${chat_id}.` });
+            res.status(200).send({ message: `Found chat ${chat_id}.`, users: chat.users.length });
           }
         });
-      };
-      mongoose.model('Chat').findOne({ id: chat_id }, (err, chat) => {
-        if (err) throw err;
-        if (!chat) {
-          res.status(404).send({ message: `Chat not found.` });
-        }
-        if (chat) {
-          res.status(200).send({ message: `Found chat ${chat_id}.`, users: chat.users.length });
-        }
-      });
+      }
     });
 
   router.route('/v1/create_chat').get(
     (req, res, next) => {
-      if (req.get('CHAT_API_SECRET_KEY') == process.env.CHAT_API_SECRET_KEY){
-        const chat_id = req.get("chat_id");
-        const chat_name = req.get("chat_name");
-        if (create_chat(chat_id, chat_name)){
-          res.send(`Created ${chat_id}.`);
-        };
-        const options = {
-          url: `${process.env.MASTER_SERVER}/api/v1/backup/create_chat`,
-          headers: {
-            'CHAT_API_SECRET_KEY': process.env.CHAT_API_SECRET_KEY,
-            'chat_id': req.get('chat_id'),
-            'chat_name': req.get('chat_name')
-          }
-        };
-        request(options, (err, response, body) => {
-          if (!err && response.statusCode == 200){
-            console.log("Created chat in sibling server");
-          }
-        });
+      if (req.get('CHAT-API-SECRET-KEY') == process.env.CHAT_API_SECRET_KEY){
+        const chat_id = req.get("CHAT-ID");
+        const chat_name = req.get("CHAT-NAME");
+        if ((chat_id === undefined) || (chat_name === undefined)){
+          res.status(400).send({ message: "Bad request."});
+        } else {
+          if (create_chat(chat_id, chat_name)){
+            res.send(`Created ${chat_id}.`);
+          };
+          const options = {
+            url: `${process.env.MASTER_SERVER}/api/v1/backup/create_chat`,
+            headers: {
+              'CHAT-API-SECRET-KEY': process.env.CHAT_API_SECRET_KEY,
+              'CHAT-ID': req.get('CHAT-ID'),
+              'CHAT-NAME': req.get('CHAT-NAME')
+            }
+          };
+          request(options, (err, response, body) => {
+            if (!err && response.statusCode == 200){
+              console.log("Created chat in sibling server");
+            }
+          });
+        }
       } else {
         res.status(401);
         const err = new Error('No Permission');
@@ -92,87 +101,94 @@ module.exports = (client, mongoose) => {
 
   router.route('/v1/backup/create_chat').post(
     (req, res, next) => {
-      if (req.get('CHAT_API_SECRET_KEY') == process.env.CHAT_API_SECRET_KEY){
-        const chat_id = req.get('chat_id');
-        const chat_name = req.get('chat_name');
-        if (create_chat(chat_id, chat_name, true)){
-          console.log(`Created chat ${chat_id}`);
-          res.status(200).send({ message: `Created chat ${chat_id} successfully.` });
-        };
+      if (req.get('CHAT-API-SECRET-KEY') == process.env.CHAT_API_SECRET_KEY){
+        const chat_id = req.get('CHAT-ID');
+        const chat_name = req.get('CHAT-NAME');
+        if ((chat_id === undefined) || (chat_name === undefined)){
+          res.status(400).send({ message: "Bad request."});
+        } else {
+          if (create_chat(chat_id, chat_name, true)){
+            console.log(`Created chat ${chat_id}`);
+            res.status(200).send({ message: `Created chat ${chat_id} successfully.` });
+          };
+        }
       };
     });
 
   router.route('/v1/join_chat').get(
     (req, res, next) => {
-      if (req.get('CHAT_API_SECRET_KEY') == process.env.CHAT_API_SECRET_KEY){
-        const chat_id = req.get('chat_id');
-        const user_id = req.get('user_id');
-        const username = req.get('username');
-        mongoose.model('Chat').findOne({ id: chat_id }, (err, chat) => {
-          if (err) throw err;
-          if (!chat) {
-            res.status(404).send({ message: 'Chat not found' });
-          } else {
-            if (chat.users.find( (x) => x.user_id === user_id )) {
-              res.status(403).send({ message: 'User already in chat' });
+      if (req.get('CHAT-API-SECRET-KEY') == process.env.CHAT_API_SECRET_KEY){
+        const chat_id = req.get('CHAT-ID');
+        const user_id = req.get('USER-ID');
+        const username = req.get('USERNAME');
+        if ((chat_id === undefined) || (user_id === undefined) || (username === undefined)){
+          res.status(400).send({ message: "Bad request."});
+        } else {
+          mongoose.model('Chat').findOne({ id: chat_id }, (err, chat) => {
+            if (err) throw err;
+            if (!chat) {
+              res.status(404).send({ message: 'Chat not found' });
             } else {
-              const ttlDate = _flooredDate(Date.now()).add(24, 'hours').unix();
-              client.zadd(["chats_ttl", ttlDate, chat_id], (err, response) => {
-                if (err) throw err;
-                client.zadd(["users_ttl", ttlDate, `${chat_id},${user_id}`], (err, response) => {
+              if (chat.users.find( (x) => x.user_id === user_id )) {
+                res.status(403).send({ message: 'User already in chat' });
+              } else {
+                const ttlDate = _flooredDate(Date.now()).add(24, 'hours').unix();
+                client.zadd(["chats_ttl", ttlDate, chat_id], (err, response) => {
                   if (err) throw err;
-                  mongoose.model('Chat').findByIdAndUpdate(chat._id, { $push: { "users": { "user_id": user_id, "username": username } } }, { safe: true, upsert: true}, (err, model) => {
+                  client.zadd(["users_ttl", ttlDate, `${chat_id},${user_id}`], (err, response) => {
                     if (err) throw err;
-                    console.log(`${user_id} joined ${chat_id}`);
-                    res.send(`${user_id} joined ${chat_id}`);
+                    mongoose.model('Chat').findByIdAndUpdate(chat._id, { $push: { "users": { "user_id": user_id, "username": username } } }, { safe: true, upsert: true}, (err, model) => {
+                      if (err) throw err;
+                      console.log(`${user_id} joined ${chat_id}`);
+                      res.send(`${user_id} joined ${chat_id}`);
+                    });
                   });
                 });
-              });
+              }
             }
-          }
-        });
-      }
-      else {
-        res.status(401);
-        const err = new Error('No Permission');
-        err.status = 401;
-        next(err);
-      }
-    });
+          });
+        };
+      } else {
+          res.status(401);
+          const err = new Error('No Permission');
+          err.status = 401;
+          next(err);
+        }
+      });
 
-  router.route('/v1/save_token').post(
-    (req, res, next) => {
-      if (req.body.CHAT_API_SECRET_KEY == process.env.CHAT_API_SECRET_KEY){
-        const token = req.body.token;
-        const user_id = req.body.user_id;
-        client.sismember(['tokens', user_id], (err, response) => {
-          if (err) throw err;
-          if (response === 0){
-            const ttlDate = _flooredDate(Date.now()).add(2, 'hours').unix();
-            client.zadd(["tokens_ttl", ttlDate, `${user_id},${username},${token}`], (err, response) => {
-              if (err) throw err;
-              client.sadd(['tokens', `${user_id}`], (err, response) => {
+/*    router.route('/v1/save_token').post(
+      (req, res, next) => {
+        if (req.body.CHAT_API_SECRET_KEY == process.env.CHAT_API_SECRET_KEY){
+          const token = req.body.token;
+          const user_id = req.body.user_id;
+          client.sismember(['tokens', user_id], (err, response) => {
+            if (err) throw err;
+            if (response === 0){
+              const ttlDate = _flooredDate(Date.now()).add(2, 'hours').unix();
+              client.zadd(["tokens_ttl", ttlDate, `${user_id},${username},${token}`], (err, response) => {
                 if (err) throw err;
-                console.log(`Added token for ${user_id}`);
+                client.sadd(['tokens', `${user_id}`], (err, response) => {
+                  if (err) throw err;
+                  console.log(`Added token for ${user_id}`);
+                });
               });
-            });
-            res.send(`Added token for ${user_id}.`);
-          } else {
-            res.status(403).send(`${user_id} already has a token.`);
-          }
-        });
-      }
-      else {
-        res.status(401);
-        const err = new Error('No Permission');
-        err.status = 401;
-        next(err);
-      }
-    });
+              res.send(`Added token for ${user_id}.`);
+            } else {
+              res.status(403).send(`${user_id} already has a token.`);
+            }
+          });
+        }
+        else {
+          res.status(401);
+          const err = new Error('No Permission');
+          err.status = 401;
+          next(err);
+        }
+      }); */
 
-  return router;
-};
+    return router;
+  };
 
-const _flooredDate = (timestamp) => {
-  return moment(timestamp).utc();
-};
+  const _flooredDate = (timestamp) => {
+    return moment(timestamp).utc();
+  };
